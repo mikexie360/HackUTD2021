976c6d064aadc86ef342e46e97179d03e0a58fc1
mikexie360 <33299123+mikexie360@users.noreply.github.com>
2021-02-27 12:42:10 -0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import random
+
+client = commands.Bot(command_prefix = ".")
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.event
+async def on_member_join(member):
+    printf(f'{member} has joined the server.')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms. ')
+
+@client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+    responses = [
+        'Yes',
+        'Maybe',
+        'No'
+    ]
+    await ctx.send(f'Question: {question} \nAnswer: {random.choice(responses)}')
+
+client.run('ODE1MjQ5NDk2NTY4MTAyOTQ1.YDpqQg.yBVkl9K1F9j5e6TOH8zekxVUDic')
+
+
